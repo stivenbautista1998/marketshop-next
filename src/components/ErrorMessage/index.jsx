60ac97bc971b9d102de99c280f1ac8c5bbe98151
Stiven Bautista <stivenb1994@gmail.com>
2022-09-26 17:23:59 -0500

    adding styles to productDetailTab component on the home page

---
+++ src/components/ErrorMessage/index.jsx
@@ -0,0 +1,11 @@
+import styles from './ErrorMessage.module.scss';
+
+const ErrorMessage = () => {
+    return (
+      <div className={styles.errorApp}>
+          Your request was not successfully. Please verify your connection, or try again later.
+      </div>
+    );
+};
+
+export { ErrorMessage };
