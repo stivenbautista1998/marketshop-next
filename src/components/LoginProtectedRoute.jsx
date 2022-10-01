dc88f1e8ce8c910fddd4f8138f260f18b7f2fd50
Stiven Bautista <stivenb1994@gmail.com>
2022-10-01 18:47:32 -0500

    fixing the access to pages that should only the accessed for logged users

--- src/components/LoginProtectedRoute.jsx
+++ src/components/LoginProtectedRoute.jsx
@@ -6,10 +6,12 @@ import { useRouter } from 'next/router';
 const LoginProtectedRoute = ({ children }) => {
   const { currentUser } = useContext(AuthContext);
   const router = useRouter();
-    // if the user already is logged in, then instead of going to login, go to home.
-    if((currentUser !== null) && (currentUser !== undefined)) router.push("/");
 
-    return children;
+  // if the user already is logged in, then instead of going to login, go to home.
+  if((currentUser !== null) && (currentUser !== undefined)) router.push("/");
+  console.log("login routing!");
+
+  return children;
 };
 
 export { LoginProtectedRoute };
--- src/components/RouteGuard.jsx
+++ src/components/RouteGuard.jsx
@@ -1,57 +1,23 @@
-import { useState, useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from "@context/AuthContext";
 import { useRouter } from 'next/router';
 
 function RouteGuard({ children }) {
   const router = useRouter();
   const { currentUser } = useContext(AuthContext);
-  const [ authorized, setAuthorized ] = useState(false);
-
-  useEffect(() => {
-    // on initial load - run auth check
-    console.log("into useEffect!");
-    authCheck(router.asPath);
-
-    // on route change start - hide page content by setting authorized to false
-    const hideContent = () => setAuthorized(false);
-    router.events.on('routeChangeStart', hideContent);
-
-    // on route change complete - run auth check
-    router.events.on('routeChangeComplete', authCheck)
-
-    // unsubscribe from events in useEffect return function
-    return () => {
-        router.events.off('routeChangeStart', hideContent);
-        router.events.off('routeChangeComplete', authCheck);
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function authCheck(url) {
-    console.log({ url });
-    console.log({ currentUser });
-
-    // redirect to login page if accessing a private page and not logged in
-    const publicPaths = ['/login'];
-    const path = url.split('?')[0];
-    if (currentUser === null && !publicPaths.includes(path)) {
-      setAuthorized(false);
-      router.push({
-        pathname: '/login',
-        query: { returnUrl: router.asPath }
-      });
-    } else {
-      setAuthorized(true);
-    }
-  }
 
   // it will enter here when the localStorage info (currentUser) is still not loaded.
-  if(currentUser === "undefined") return <h2>Loading...</h2>;
-
-  // it will enter here when the info currentUser is obtained, therefore authorized is defined.
-  console.log({ authorized });
-  return (authorized && children);
+  if(currentUser === undefined) return <h2>Loading Page...</h2>;
+  // it will enter here when the the user is not logged in.
+  if(currentUser === null) {
+    router.push({
+      pathname: '/login',
+      query: { returnUrl: router.asPath }
+    });
+  } else {
+    // enter here when the user is logged correctly, in other words, currentUser has the user's info.
+    return children;
+  }
 }
 
 export { RouteGuard };
